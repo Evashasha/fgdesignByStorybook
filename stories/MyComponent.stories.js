5f15d849f1af5aebb6de98acac991655a93e143a
evalli <evalli@tencent.com>
2024-01-02 17:43:16 +0800

    关联figma设计稿

---
+++ stories/MyComponent.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// More on default export: https://storybook.js.org/docs/writing-stories/#default-export
+const meta = {
+  component: ()=><></>,
+};
+
+export default meta;
+
+export const Example = {
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/yxKD85Kc0qPYlOqW4ILucw/Untitled?type=design&node-id=1%3A3&mode=design&t=WGXTv1zQCmfFdOVZ-1',
+    },
+  },
+};
